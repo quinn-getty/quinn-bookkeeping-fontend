358813516cff6485f5a59b24445ef1760508b4ce
quinn-getty <quinnn.gao@gmail.com>
2022-10-06 01:36:47 +0800

    feat: 使用css module

--- src/pages/welcome/index.tsx
+++ src/pages/welcome/index.tsx
@@ -1,10 +1,11 @@
 import { defineComponent } from 'vue';
 import { RouterView } from 'vue-router';
+import style from './welcome.module.scss'
 
 export const Welcome = defineComponent({
   setup: (props, context) => {
     return () => (
-      <div>welcome
+      <div class={style.wrapper}>welcome
         <RouterView />
       </div>
     )
