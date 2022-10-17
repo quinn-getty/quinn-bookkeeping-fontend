6aad43f170fd059136d81c5c2e2a45f52276c5a3
quinn-getty <quinnn.gao@gmail.com>
2022-10-17 11:19:38 +0800

    feat: 添加creat item 布局

--- src/pages/item/create/index.tsx
+++ src/pages/item/create/index.tsx
@@ -1,15 +1,25 @@
 import { defineComponent, ref, PropType } from "vue";
+import { useRouter } from "vue-router";
+import IconSvg from "../../../components/icon";
+import MainLayout from "../../../components/main-layout";
+import style from './index.module.scss'
 
 export const ItemCreate = defineComponent({
-  props: {
-    // key: {
-    //   type: String as PropType<T>
-    // }
-  },
 
   setup(props, content) {
+    const router = useRouter()
+    const goBack = () => {
+      router.go(-1)
+    }
     return () => <>
-      create
+      <MainLayout>
+        {{
+          icon: () => <IconSvg name="left" class={style.menu_icon} onClick={goBack} />,
+          title: () => <>记一笔</>,
+          default: () => <>
+            xxx</>
+        }}
+      </MainLayout>
     </>
   }
 })
--- src/pages/item/index.tsx
+++ src/pages/item/index.tsx
@@ -2,11 +2,6 @@ import { defineComponent, ref, PropType } from "vue";
 import { RouterView } from "vue-router";
 
 export const ItemPage = defineComponent({
-  props: {
-    // key: {
-    //   type: String as PropType<T>
-    // }
-  },
 
   setup(props, content) {
     return () => <>
