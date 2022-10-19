d4d4782239bc93f71cc31a59ab45895a99598a21
quinn-getty <quinnn.gao@gmail.com>
2022-10-19 08:47:37 +0800

    feat: 表单验证的功能

---
+++ src/shared/validata.ts
@@ -0,0 +1,54 @@
+interface FormDataType {
+  [_: string]: string | number | null | undefined | FormDataType
+}
+
+type FNType = string | number | null | undefined | FormDataType
+
+export type RulesType<T> = RuleType<T>[]
+type RuleType<T> = {
+  key: keyof T;
+  message?: string;
+  required?: true;
+  regexp?: RegExp;
+  fn?: <F>(_: F) => { status: boolean, message?: string }
+}
+
+type ErrorsType<T> = {
+  [k in keyof T]?: string[]
+}
+
+/**
+ *      
+ * @param formData 
+ * 
+ * @param rules
+ *  required > pettern > fn
+ */
+export const validata = <T>(formData: T, rules: RulesType<T>) => {
+  const errors: ErrorsType<T> = {}
+  rules.forEach(item => {
+    const { key } = item
+    const value = formData[key]
+    let isError = false // 
+    let message = item?.message || ''
+
+    if (item?.required) {
+      isError = !value
+    } else if (item?.regexp) {
+      console.log('regexp', !item.regexp.test((value || '').toString()))
+
+      isError = !item.regexp.test((value || '').toString())
+    } else if (item?.fn) {
+      const { status, message: msg } = item.fn(value)
+      isError = !status
+      message = msg || message
+    }
+
+    if (isError) {
+      errors[key] = errors[key] ?? []
+      errors[key]?.push(message)
+    }
+  })
+  return errors
+}
+export default validata
