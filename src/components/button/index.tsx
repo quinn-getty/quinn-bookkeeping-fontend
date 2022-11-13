dd424efb7224d5f5620a60d923f077b13471f366
quinn-getty <quinnn.gao@gmail.com>
2022-11-13 21:48:25 +0800

    fix: 倒计时 60 秒

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -13,11 +13,16 @@ export const Button = defineComponent({
     type: {
       type: String as PropType<'submit' | 'button'>,
       default: 'button'
+    },
+    disabled: {
+      type: Boolean,
+      default: false
     }
+    
   },
   setup: (props, context) => {
     return () => (
-      <button type={props.type} class={[style.button, style[props.level]]} onClick={props.onClick}>
+      <button disabled={props.disabled} type={props.type} class={[style.button, style[props.level]]} onClick={props.onClick}>
         {context.slots.default?.()}
       </button>
     )
--- src/pages/sign/index.tsx
+++ src/pages/sign/index.tsx
@@ -31,6 +31,12 @@ export const SignInPage = defineComponent({
     const goBack = () => {
       router.go(-1)
     }
+
+    const onClickSendValidationCode = async () => {
+      // const response = await axios.post('/api/v1/validation_codes', { email: formData.email })
+      // console.log(response)
+    }
+
     return () => (
       <MainLayout>{
         {
@@ -48,7 +54,10 @@ export const SignInPage = defineComponent({
                   v-model={formData.email} error={errors.email?.[0]} />
                 <FormItem label="验证码" type="validationCode"
                   placeholder='请输入六位数字'
+                  countFrom={60}
+                  onClick={onClickSendValidationCode}
                   v-model={formData.code} error={errors.code?.[0]} />
+                  
                 <FormItem style={{ paddingTop: '96px' }}>
                   <Button >登录</Button>
                 </FormItem>
